6fe56653b6c620a09afb103942fdaa3828dce0be
Suchindra Chandrahas <45355851+suchindrac@users.noreply.github.com>
2025-08-29 18:27:34 +0530

    Add files via upload

--- neuron/neuron.py
+++ neuron/neuron.py
@@ -2,7 +2,6 @@ import pprint
 import sys
 import traceback
 import string
-from itertools import chain
 
 nouns_list = []
 verbs_list = []
@@ -10,18 +9,12 @@ verbs_list = []
 class Neuron:
     def __init__(self, text, node, prev_node, next_node):
         self.node = None
-        self.next_nodes = []
-        self.prev_nodes = []
+        self.next_chains = []
+        self.prev_chains = []
         if not node:
             self.node = self
 
         self.text = text
-        if prev_node:
-            self.prev_nodes.append(prev_node)
-                
-        if next_node:
-            self.next_nodes.append(next_node)
-
         self.conn_nouns = []
         self.conn_verbs = []
         self.weightage = 1
@@ -40,8 +33,14 @@ class NodeMap():
     def display(self):
         for key in self.key_map.keys():
             node = self.key_map[key]
-            prev_text = [x.text for x in node.prev_nodes]
-            next_text = [x.text for x in node.next_nodes]
+            prev_text = []
+            next_text = []
+            for chain in node.prev_chains:
+                prev_text.append([x.text for x in chain])
+
+            for chain in node.next_chains:
+                next_text.append([x.text for x in chain])
+
             conn_nouns = [x.text for x in node.conn_nouns]
             conn_verbs = [x.text for x in node.conn_verbs]
             print(f"{node.text} -> {prev_text} {next_text} {conn_nouns} {conn_verbs}")
@@ -65,22 +64,21 @@ class NodeMap():
 
         verb_nodes = [self.get_node(x) for x in verbs_list_local]
         verb_nodes = [x for x in verb_nodes if x != False]
-        
-        return list(set(chain.from_iterable(x.conn_nouns for x in verb_nodes)))
-        
-    def add_phrase(self, phrase):
-        phrase = phrase.split()
-        phrase = [x for x in phrase if x.isalnum()]
-        phrase = [x.strip(string.punctuation).lower() for x in phrase]
-        
-        for word in phrase:
-            self.add_node(word)
-            
+
+        next_chain_verbs = []
+        for node in verb_nodes:
+            for chain in node.next_chains:
+                next_chain_verbs.append([x.text for x in chain])
+
+        return next_chain_verbs 
+
+    def append_chain(self, phrase):
         verbs = [x for x in verbs_list if x in phrase]
         verb_nodes = [self.key_map[x] for x in verbs]
         
         nouns = [x for x in nouns_list if x in phrase]
         noun_nodes = [self.key_map[x] for x in nouns]
+        print(f"Noun nodes: {[x.text for x in noun_nodes]}")
         
         nodes_list = [self.key_map[x] for x in phrase]
     
@@ -90,19 +88,28 @@ class NodeMap():
                 nprev, ncur, nnext = next(generator)
            
                 if nprev:
-                    ncur.prev_nodes = nprev
+                    ncur.prev_chains.extend([nprev])
                 if nnext:
-                    ncur.next_nodes = nnext
+                    ncur.next_chains.extend([nnext])
             except:
                 break
                 
         for node in verb_nodes:
-            node.conn_nouns = noun_nodes
+            node.conn_nouns.extend(noun_nodes)
 
         for node in noun_nodes:
-            node.conn_verbs = verb_nodes
+            node.conn_verbs.extend(verb_nodes)
 
-            
+    def add_phrase(self, phrase):
+        phrase = phrase.split()
+        phrase = [x for x in phrase if x.isalnum()]
+        phrase = [x.strip(string.punctuation).lower() for x in phrase]
+        
+        for word in phrase:
+            self.add_node(word)
+
+        self.append_chain(phrase)
+
 if __name__ == "__main__":
     fd = open('nouns.txt', 'r')
     nouns_list = fd.read()
@@ -118,12 +125,49 @@ if __name__ == "__main__":
 
     nmap = NodeMap()
 
-    nmap.add_phrase("I like playing tennis")
-    nmap.display()
-    answers = nmap.ask("What do I like?")
-
-    print([x.text for x in answers])
-    
     # g = get_node_chain("I like playing tennis".split())
     # while True:
     #     print(next(g))
+
+    fd = open('nouns.txt', 'r')
+    nouns_list = fd.read()
+    nouns_list = nouns_list.split("\n")
+    nouns_list = [x for x in nouns_list if x != ""]
+    fd.close()
+
+    fd = open('verbs.txt', 'r')
+    verbs_list = fd.read()
+    verbs_list = verbs_list.split("\n")
+    verbs_list = [x for x in verbs_list if x != ""]
+    fd.close()
+
+    nmap = NodeMap()
+
+    # g = get_node_chain("I like playing tennis".split())
+    # while True:
+    #     print(next(g))
+
+    while True:
+        query_or_data = input("> ")
+        if "?" in query_or_data:
+            answers = nmap.ask(query_or_data)
+            print([x for x in answers])
+        else:
+           nmap.add_phrase(query_or_data)
+           nmap.display()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
